406cf8df7fcfac23101b7ffb49cf7f75868acbc8
DannyMagno <117946519+DannyMagno@users.noreply.github.com>
2025-07-29 11:58:45 -0600

    Add files via upload

---
+++ mock/players.js
@@ -0,0 +1,38 @@
+export const defaultPlayers = [
+  {
+    id: 'p1',
+    name: 'Lionel Messi',
+    position: 'Delantero',
+    stats: { attack: 95, defense: 30, speed: 90 }
+  },
+  {
+    id: 'p2',
+    name: 'Cristiano Ronaldo',
+    position: 'Delantero',
+    stats: { attack: 93, defense: 28, speed: 88 }
+  },
+  {
+    id: 'p3',
+    name: 'Virgil van Dijk',
+    position: 'Defensa',
+    stats: { attack: 40, defense: 92, speed: 75 }
+  },
+  {
+    id: 'p4',
+    name: 'Kevin De Bruyne',
+    position: 'Mediocampista',
+    stats: { attack: 88, defense: 65, speed: 80 }
+  },
+  {
+    id: 'p5',
+    name: 'Kylian Mbappé',
+    position: 'Delantero',
+    stats: { attack: 92, defense: 35, speed: 97 }
+  },
+  {
+    id: 'p6',
+    name: 'Manuel Neuer',
+    position: 'Portero',
+    stats: { attack: 20, defense: 90, speed: 60 }
+  }
+];
