342abb471c72b1367c4e51fec0238acdad9ffcb6
DannyMagno <117946519+DannyMagno@users.noreply.github.com>
2025-07-29 11:44:24 -0600

    Add files via upload

---
+++ src/mock/movies.js
@@ -0,0 +1,93 @@
+export const defaultMovies = [
+  {
+    id: 'm1',
+    title: 'El Gran Nerd',
+    description: 'La épica historia de un genio de la programación que salva el mundo con React y TailwindCSS, mientras hace bromas sarcásticas.',
+    image: 'https://image.tmdb.org/t/p/w500/qW1g0y0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g.jpg', // Placeholder image
+    rating: 9.8,
+    genre: 'Comedia'
+  },
+  {
+    id: 'm2',
+    title: 'La Venganza del Código',
+    description: 'Un thriller tecnológico donde un código malicioso amenaza con borrar todas las bases de datos, y solo un equipo de desarrolladores puede detenerlo.',
+    image: 'https://image.tmdb.org/t/p/w500/qW1g0y0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g.jpg', // Placeholder image
+    rating: 8.5,
+    genre: 'Acción'
+  },
+  {
+    id: 'm3',
+    title: 'El Algoritmo del Amor',
+    description: 'Una comedia romántica donde dos programadores intentan encontrar el amor verdadero usando algoritmos complejos, con resultados hilarantes.',
+    image: 'https://image.tmdb.org/t/p/w500/qW1g0y0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g.jpg', // Placeholder image
+    rating: 7.9,
+    genre: 'Romance'
+  },
+  {
+    id: 'm4',
+    title: 'Matrix Recargada (con Tailwind)',
+    description: 'Neo descubre que la Matrix fue construida con CSS en línea y decide reescribirla con TailwindCSS para una experiencia más fluida.',
+    image: 'https://image.tmdb.org/t/p/w500/qW1g0y0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g.jpg', // Placeholder image
+    rating: 9.0,
+    genre: 'Ciencia Ficción'
+  },
+  {
+    id: 'm5',
+    title: 'Interestelar (con React Hooks)',
+    description: 'Un grupo de astronautas viaja a través de un agujero de gusano para encontrar un nuevo hogar, descubriendo que el universo es un gran componente funcional.',
+    image: 'https://image.tmdb.org/t/p/w500/qW1g0y0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g.jpg', // Placeholder image
+    rating: 9.5,
+    genre: 'Ciencia Ficción'
+  },
+  {
+    id: 'm6',
+    title: 'El Silencio de los Servidores',
+    description: 'Una detective persigue a un asesino en serie que deja un rastro de servidores caídos y bases de datos corruptas.',
+    image: 'https://image.tmdb.org/t/p/w500/qW1g0y0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g.jpg', // Placeholder image
+    rating: 8.7,
+    genre: 'Drama'
+  },
+  {
+    id: 'm7',
+    title: 'La Red Social (versión React)',
+    description: 'La historia de cómo se creó la red social más grande del mundo, pero esta vez, con todos los componentes y hooks de React.',
+    image: 'https://image.tmdb.org/t/p/w500/qW1g0y0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g.jpg', // Placeholder image
+    rating: 8.2,
+    genre: 'Drama'
+  }
+];
+
+export const defaultSeries = [
+  {
+    id: 's1',
+    title: 'Stranger Bugs',
+    description: 'Un grupo de niños descubre que los errores en el código no son tan aleatorios como parecen, y que hay algo más allá del debugger.',
+    image: 'https://image.tmdb.org/t/p/w500/qW1g0y0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g.jpg', // Placeholder image
+    rating: 9.1,
+    genre: 'Ciencia Ficción'
+  },
+  {
+    id: 's2',
+    title: 'The Crown (de CSS)',
+    description: 'Un drama histórico sobre la monarquía británica, pero contada a través de la evolución de las hojas de estilo en cascada.',
+    image: 'https://image.tmdb.org/t/p/w500/qW1g0y0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g.jpg', // Placeholder image
+    rating: 8.8,
+    genre: 'Drama'
+  },
+  {
+    id: 's3',
+    title: 'Breaking Bad (con JavaScript)',
+    description: 'Un profesor de química se convierte en un capo de la programación de JavaScript para asegurar el futuro de su familia.',
+    image: 'https://image.tmdb.org/t/p/w500/qW1g0y0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g.jpg', // Placeholder image
+    rating: 9.9,
+    genre: 'Drama'
+  },
+  {
+    id: 's4',
+    title: 'Friends (en un entorno de desarrollo)',
+    description: 'Seis amigos intentan sobrevivir a la vida adulta mientras comparten un apartamento y un entorno de desarrollo compartido.',
+    image: 'https://image.tmdb.org/t/p/w500/qW1g0y0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g0g.jpg', // Placeholder image
+    rating: 9.0,
+    genre: 'Comedia'
+  }
+];
